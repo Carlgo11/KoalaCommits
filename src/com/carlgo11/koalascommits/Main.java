96564a867e2aa2e3be799ddee81c3d4570d0fbb8
Carlgo11 <Carlgo11@portalcraft.se>
2014-01-09 22:36:32 +0100

    firstcommit
    Meanwhile Koala commited 999999999 commits

---
+++ src/com/carlgo11/koalascommits/Main.java
@@ -0,0 +1,13 @@
+package com.carlgo11.koalascommits;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        System.out.println("Koala's commits since this program started. (Live data)");
+        for(int i = 0; 1000000000 != i; i++){
+        System.out.println("Commits by Koala: "+i);
+        }
+    }
+    
+}
